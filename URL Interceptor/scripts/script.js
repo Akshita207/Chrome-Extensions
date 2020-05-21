cda54f4a8b7d95543a8f74e6f9870f326eb9267d
Akshita <akshitajain207@gmail.com>
2020-05-22 00:12:29 +0530

    url interceptor

---
+++ URL Interceptor/scripts/script.js
@@ -0,0 +1,21 @@
+let urlsToBlock = ["https://www.facebook.com"];
+
+chrome.runtime.onMessage.addListener(
+	function(request){
+		if(request.addUrl){
+			urlsToBlock.push(request.addUrl)
+		}
+	});
+
+chrome.webRequest.onBeforeRequest.addListener(
+	function(details) {
+		console.log(details);
+		if(urlsToBlock.includes(details.initiator)){
+			return {cancel: true}
+		} else {
+			return {cancel: false}
+		}
+	}, 
+	{urls: ["<all_urls>"]}, 
+	["blocking"]
+)
